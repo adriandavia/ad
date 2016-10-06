553a756ff62d1611102756946cd67cee46071651
adriandavia <dam.adavia@institutoserpis.org>
2016-10-06 10:31:31 +0200

    Añadido PTreeView y la librería Org.InstitutoSerpis.Org

--- PTreeView/PTreeView/MainWindow.cs
+++ PTreeView/PTreeView/MainWindow.cs
@@ -1,9 +1,6 @@
-using System;
 using Gtk;
 using System;
-using Gtk;
-using System.Data;
-using MySql.Data.MySqlClient;
+
 using Org.InstitutoSerpis.Ad;
 
 public partial class MainWindow: Gtk.Window
@@ -11,11 +8,11 @@ public partial class MainWindow: Gtk.Window
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
-		string[] columnNames = {"id","nombre","precio"};
-		TreeViewHelper.AppendColumns (treeView, columnNames);
-		ListStore listStore = new ListStore (typeof(long), typeof(string), typeof(decimal), typeof(long));
-		listStore.AppendValues (1L, "categoria 1", 1.50);
-		listStore.AppendValues (1L, "categoria 2", 1.50);
+		TreeViewHelper.AppendColumns (treeView, new string[] { "id", "nombre", "precio" });
+		ListStore listStore = new ListStore (typeof(long), typeof(string), typeof(decimal));
+		listStore.AppendValues (1L, "artículo 1", 1.5m);
+		listStore.AppendValues (2L, "artículo 2", 2.5m);
+		treeView.Model = listStore;
 	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
--- PTreeView/TreeViewHelper/TreeViewHelper.cs
+++ PTreeView/TreeViewHelper/TreeViewHelper.cs
@@ -5,10 +5,10 @@ namespace Org.InstitutoSerpis.Ad
 {
 	public class TreeViewHelper
 	{
-		public static void AppendColumns (TreeViewHelper treeView, string[] columnNames){
+		public static void AppendColumns (TreeView treeView, string[] columnNames){
 			foreach (string columnName in columnNames) {
 				treeView.AppendColumn (columnName, new CellRendererText (),
-                   delegate (TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter ArgIterator) {
+                   delegate (TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter iter) {
 						int column = Array.IndexOf (treeView.Columns, tree_column);
 						CellRendererText CellRendererText = (CellRendererText)cell;
 						object value = tree_model.GetValue (iter, column);
