9c738aaf7e6b57c463f568e4da4975d0723f6c44
adriandavia <dam.adavia@institutoserpis.org>
2016-10-06 09:05:24 +0200

    PTreeView

--- PTreeView/PTreeView/MainWindow.cs
+++ PTreeView/PTreeView/MainWindow.cs
@@ -1,25 +1,23 @@
 using System;
 using Gtk;
+using System;
+using Gtk;
 using System.Data;
 using MySql.Data.MySqlClient;
 using Org.InstitutoSerpis.Ad;
 
-//FORMA DE PARTICULO DEL PROFESOR
 public partial class MainWindow: Gtk.Window
 {	
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
-		string[] columnNames = {"id", "nombre", "precio"};
-		TreeViewHelper.AppendColumns(treeview, columnNames);
-		//el rypeof(decimal) no es reconocido por el TreeView
+		string[] columnNames = {"id","nombre","precio"};
+		TreeViewHelper.AppendColumns (treeView, columnNames);
 		ListStore listStore = new ListStore (typeof(long), typeof(string), typeof(decimal), typeof(long));
-
-		treeView.Model = listStore;
-
 		listStore.AppendValues (1L, "categoria 1", 1.50);
-		listStore.AppendValues (1L, "categoria 1", 1.50);
+		listStore.AppendValues (1L, "categoria 2", 1.50);
 	}
+
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
 	{
 		Application.Quit ();
---
+++ PTreeView/TreeViewHelper/TreeViewHelper.cs
@@ -0,0 +1,22 @@
+using System;
+using Gtk;
+
+namespace Org.InstitutoSerpis.Ad
+{
+	public class TreeViewHelper
+	{
+		public static void AppendColumns (TreeViewHelper treeView, string[] columnNames){
+			foreach (string columnName in columnNames) {
+				treeView.AppendColumn (columnName, new CellRendererText (),
+                   delegate (TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter ArgIterator) {
+						int column = Array.IndexOf (treeView.Columns, tree_column);
+						CellRendererText CellRendererText = (CellRendererText)cell;
+						object value = tree_model.GetValue (iter, column);
+						CellRendererText.Text = value.ToString ();
+					}
+				);
+			}
+		}
+	}
+}
+
